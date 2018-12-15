ccdae9cc55fbb7ceaf7cc3d57443bcceb2399dd6
Matt Briney <mkbriney@gmail.com>
2018-12-14 21:21:14 -0500

    Completed game

--- js/script.js
+++ js/script.js
@@ -4,6 +4,33 @@ function hidePanels(){
 	$(".mv_panel").hide();
 }
 
+var confettiPlayers = [];
+
+function makeItConfetti() {
+	var confetti = document.querySelectorAll('.confetti');
+	
+	if (!confetti[0].animate) {
+		return false;
+	}
+
+	for (var i = 0, len = confetti.length; i < len; ++i) {
+		var snowball = confetti[i];
+		snowball.innerHTML = '<div class="rotate"><div class="askew"></div></div>';
+		var scale = Math.random() * .8 + .2;
+		var player = snowball.animate([
+			{ transform: 'translate3d(' + (i/len*100) + 'vw,0,0) scale(' + scale + ')', opacity: scale },
+			{ transform: 'translate3d(' + (i/len*100 + 10) + 'vw,100vh,0) scale(' + scale + ')', opacity: 1 }
+		], {
+			duration: Math.random() * 3000 + 3000,
+			iterations: Infinity,
+			delay: -(Math.random() * 5000)
+		});
+		
+		
+		confettiPlayers.push(player);
+	}
+}
+
 
 // Startup scripts
 $( document ).ready(function() {
@@ -107,4 +134,21 @@ $( document ).ready(function() {
 			$("#error").show();
 		}
 	});	
+
+	$( "#mv_action_10" ).bind( "click", function() {
+		if ($("#mv_answer_10a").val().toLowerCase() == "his" && $("#mv_answer_10b").val().toLowerCase() == "co" && $("#mv_answer_10c").val().toLowerCase() == "ntry"){
+			$(".surpress").hide();
+			$("#last_title").text("Huzzah! You Got it Right!");
+			makeItConfetti();
+		} else {
+			$("#error").show();
+		}
+	});		
+	
+
+
+	
+	
 });
+
+
